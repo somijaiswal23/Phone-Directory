92df6fb5e14660d41415d60b32770b8bcaf13947
somijaiswal23 <somi.jaiswal@iiitb.net>
2019-02-17 12:00:50 +0530

    Added subscribers

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css';
+
+const Header = function (props) {
+    //const headerStyle = {textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase'};
+    return(
+        <div className="header">
+            {props.heading}
+        </div>
+    )
+}
+
+// class Header extends Component{
+//     render(){
+//         return(
+//             <div className={"header"}>
+//                 Phone Directory
+//             </div>
+//         )
+//     }
+// }
+
+export default Header;
